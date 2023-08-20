0f69a1405f41d117ad9b411476826f5bffc95e2c
IAmJakPro <jakdev20@gmail.com>
2023-08-20 11:33:26 +0100

    add pro rtl

--- app/[lang]/checkout/components/CheckoutError.tsx
+++ app/[lang]/checkout/components/CheckoutError.tsx
@@ -9,7 +9,7 @@ export default async function CheckoutError({ lang }: any) {
   return (
     <div className="bg-secondary pt-[6rem]">
       <div className="flex items-center justify-center flex-col">
-        <XCircleIcon className="h-10 w-10 text-danger" />
+        <XCircleIcon className="h-20 w-20 text-danger" />
         <div className="bg-white sm:w-[400px] w-full mt-12 rounded-t-3xl h-screen p-3 pt-6">
           <div className="text-lg font-bold text-center">
             {translate(dict, 'checkout_error')}
